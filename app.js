623ee0a04163523c2112cfad65729aef5a9707a0
Nathan Otto <ottoetc@gmail.com>
2016-07-17 21:25:56 -0700

    Add POST request functionality

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 const pgp = require('pg-promise')();
+const bodyParser = require('body-parser');
 const connection = process.env.DATABASE_URL || 'postgres://localhost:5432/blog';
 const db = pgp(connection);
 
 const port = process.env.PORT || 8080;
 const router = express.Router();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/api/v1/', router);
 
 router.get('/', function (req, res) {
   res.send('Hello from the API');
@@ -25,9 +30,8 @@ router.get('/posts', function (req, res, next) {
     });
 });
 
-router.get('/posts/:id', function (req, res) {
+router.get('/posts/:id', function (req, res, next) {
   let postId = parseInt(req.params.id);
-  console.log("postId: ", postId);
   db.one('select * from posts where id = $1', postId)
     .then(function (data) {
       res.status(200)
@@ -41,8 +45,17 @@ router.get('/posts/:id', function (req, res) {
     });
 });
 
-router.post('/posts', function (req, res) {
-  res.send('POST create blog post');
+router.post('/posts', function (req, res, then) {
+  db.query('insert into posts(title, content) values(${title}, ${content})', req.body)
+    .then(function () {
+      res.status(200)
+        .json({
+          status: 'success',
+          message: 'Posted a blog post'
+        });
+    }).catch(function (err) {
+      return next(err);
+    });
 });
 
 router.put('/posts/:id', function (req, res) {
@@ -53,7 +66,6 @@ router.delete('/posts/:id', function (req, res) {
   res.send('DELETE a blog post');
 });
 
-app.use('/api/v1/', router);
 
 app.listen(port);
 console.log('API is listening on localhost:' + port);
