86d69d7e544a6d904d68473026e3878ac5a1d1f5
Nathan Otto <ottoetc@gmail.com>
2016-07-17 19:59:11 -0700

    Add PostgreSQL connection and GET functionality

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const app = express();
+const pgp = require('pg-promise')();
+const connection = process.env.DATABASE_URL || 'postgres://localhost:5432/blog';
+const db = pgp(connection);
 
 const port = process.env.PORT || 8080;
 const router = express.Router();
@@ -8,12 +11,34 @@ router.get('/', function (req, res) {
   res.send('Hello from the API');
 });
 
-router.get('/posts', function (req, res) {
-  res.send('GET all blog posts');
+router.get('/posts', function (req, res, next) {
+  db.any('select * from posts')
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Got all posts'
+        });
+    }).catch(function (err) {
+      return next(err);
+    });
 });
 
 router.get('/posts/:id', function (req, res) {
-  res.send('GET single blog post');
+  let postId = parseInt(req.params.id);
+  console.log("postId: ", postId);
+  db.one('select * from posts where id = $1', postId)
+    .then(function (data) {
+      res.status(200)
+        .json({
+          status: 'success',
+          data: data,
+          message: 'Got single post by id'
+        });
+    }).catch(function (err) {
+      return next(err);
+    });
 });
 
 router.post('/posts', function (req, res) {
