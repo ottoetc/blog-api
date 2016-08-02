91359bb3be40f1293c54b14189e6b5f0bf368fc4
Nathan Otto <ottoetc@gmail.com>
2016-08-01 23:37:10 -0700

    Add bootstrap styling to buttons and forms

--- ember-app/app/components/new-post.js
+++ ember-app/app/components/new-post.js
@@ -6,15 +6,18 @@ export default Ember.Component.extend({
     postFormShow() {
       this.set('addNewPost', true);
     },
-
+    
     save1() {
       var params = {
         title: this.get('title'),
         content: this.get('content'),
       };
+      // Clear form values
       this.set('title', '');
       this.set('content', '');
+      // Hide form
       this.set('addNewPost', false);
+      // Send save action up to the parent component
       this.sendAction('save2', params);
     }
   }
